8ffe04c70fd47a7a604155ea1c8d1822967fec8b
sathish-RG <sathish958527@gmail.com>
2025-04-25 19:35:23 +0530

    webhooks added

--- be-bgremover/controllers/userController.js
+++ be-bgremover/controllers/userController.js
@@ -1,6 +1,66 @@
+// import { webhook } from 'svix';
+import pkg from 'svix';
+const { webhook } = pkg;
+import userModel from '../models/userModel.js';
 //api controller function to mange clerk user with database
 //http://localhost:4000/api/user/webhooks
 
 const clerkWebhooks=async (req,res)=>{
   
+    try{
+     //create a svix instance with clerk webhook secret.
+     const whook=new webhook(process.env.CLERK_WEB_HOOK_SECRET)
+
+      await whook.verify(JSON.stringify(req.body),{
+        "svix-id":req.headrs["svix-id"],
+        "svix-timestamp":req.headrs["svix-timestamp"],
+        "svix-signature":req.headers["svix-signature"]
+      })
+       
+     const{data,type}=req.body
+
+     switch(type){
+      case "user.created":{
+         
+        const userData={
+          clerkId:data.id,
+          email:data.email_address[0].email_address,
+          firstname:data.first_name,
+          lastname:data.last_name,
+          photo:data.image_url,
+
+        }
+        await userModel.create(userData)
+        res.json({})
+
+        break;
+      }
+      case "user.updated":{
+        const userData={
+          email:data.email_address[0].email_address,
+          firstname:data.first_name,
+          lastname:data.last_name,
+          photo:data.image_url,
+
+        }
+        await userModel.findOneAndUpdate({clerkId:data.id},userData)
+        res.json({})
+        break;
+      }
+      case "user.deleted":{
+        await userModel.findOneAndDelete({clerkId:data.id})
+        res.json({})
+        break;
+      }
+        
+        default:
+          break;
+     }
+
+    }catch(error){
+      console.log(error.message)
+      res.json({success:false, message:error.message})
+    }
+
 }
+export {clerkWebhooks}
--- be-bgremover/server.js
+++ be-bgremover/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/mongodb.js';
+import userRouter from './routes/userRoutes.js';
 
 //app config
 const PORT=process.env.PORT || 4000
@@ -14,5 +15,6 @@ app.use(cors())
 
 //api route
 app.get('/',(req,res)=>res.send('API Working'));
+app.use('/api/user',userRouter)
 
 app.listen(PORT,()=>console.log('Server Running On Port '+PORT))
