c7257a49453a48106276d071e5aaa9c2f75ca8d6
sathish-RG <sathish958527@gmail.com>
2025-04-25 18:32:30 +0530

    first commit

---
+++ be-bgremover/controllers/userController.js
@@ -0,0 +1,6 @@
+//api controller function to mange clerk user with database
+//http://localhost:4000/api/user/webhooks
+
+const clerkWebhooks=async (req,res)=>{
+  
+}
---
+++ be-bgremover/server.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './config/mongodb.js';
+
+//app config
+const PORT=process.env.PORT || 4000
+const app=express();
+await connectDB()
+
+//Intialize Middlewares
+app.use(express.json())
+app.use(cors())
+
+//api route
+app.get('/',(req,res)=>res.send('API Working'));
+
+app.listen(PORT,()=>console.log('Server Running On Port '+PORT))
