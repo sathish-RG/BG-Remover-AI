c2062ae36618a06fffdb16e37ea302e865265802
sathish-RG <sathish958527@gmail.com>
2025-04-26 08:59:05 +0530

    headers

--- be-bgremover/controllers/userController.js
+++ be-bgremover/controllers/userController.js
@@ -11,8 +11,8 @@ const clerkWebhooks=async (req,res)=>{
      const whook=new Webhook(process.env.CLERK_WEB_HOOK_SECRET)
 
       await whook.verify(JSON.stringify(req.body),{
-        "svix-id":req.headrs["svix-id"],
-        "svix-timestamp":req.headrs["svix-timestamp"],
+        "svix-id":req.headers["svix-id"],
+        "svix-timestamp":req.headers["svix-timestamp"],
         "svix-signature":req.headers["svix-signature"]
       })
        
